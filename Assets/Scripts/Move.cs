5dd7ed47abdcbbb052f804b9e91e57019792b75f
lumilo8 <lumilo8@gmail.com>
2014-11-10 23:27:53 +0100

    Moving player

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,37 +2,20 @@
 using System.Collections;
 
 public class Move : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.x--;
-			this.transform.position = position;
+	public float speed = 1f;
+	void Update ()
+	{
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.position += new Vector3 (speed * Time.deltaTime, 0.0f, 0);
 		}
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.x++;
-			this.transform.position = position;
+		else if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0);
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y++;
-			this.transform.position = position;
+		else if(Input.GetKey(KeyCode.UpArrow)){
+			transform.position += new Vector3 (0.0f, speed * Time.deltaTime, 0);
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y--;
-			this.transform.position = position;
+		else if(Input.GetKey(KeyCode.DownArrow)){
+			transform.position -= new Vector3 (0.0f, speed * Time.deltaTime, 0);
 		}
 	}
 }
