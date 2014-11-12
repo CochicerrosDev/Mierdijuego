b79aa52e923a15d2d6a002505f1522a29c00c8c7
lumilo8 <lumilo8@gmail.com>
2014-11-12 18:43:42 +0100

    Refactoring

---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Entity : MonoBehaviour {
+
+	public float speed;
+	public Animator animador;
+	public float vely = 0f;
+	public float velx = 0f;
+
+	void Awake(){
+		
+		animador = GetComponent<Animator> ();
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Entity/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velx = 0;
+		vely = 0;
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			velx = 1;
+			rigidbody2D.transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			velx = -1;
+			rigidbody2D.transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			vely = 1;
+			rigidbody2D.transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {						
+			vely = -1;
+			rigidbody2D.transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		animador.SetFloat ("VelX", velx);
+		animador.SetFloat ("VelY", vely);
+	}
+}
