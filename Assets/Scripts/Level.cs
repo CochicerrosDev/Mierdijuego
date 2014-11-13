76e3c48b66f35195bb8ea0922a2afbc9e102ed50
lumilo8 <lumilo8@gmail.com>
2014-11-13 18:06:50 +0100

    gogo

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+
+	private int width;
+	private int height;
+
+	public Transform grass;
+	public Transform stone;
+
+	private Color[] colours;
+
+	public Color grassColor;
+	public Color stoneColor;
+	public Color spawn;
+
+	public Texture2D levelTexture;
+
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		width = levelTexture.width;
+		height = levelTexture.height;
+		loadLevel();
+	}
+
+	void loadLevel() {
+		colours = new Color[width * height];
+		colours = levelTexture.GetPixels();
+
+		for (int y=0; y<height; y++) {
+			for(int x=0;x<width;x++) {
+				if(colours[x+y*width] == grassColor){
+					Instantiate(grass, new Vector3(x,y), Quaternion.identity);
+				}
+				if(colours[x+y*width] == stoneColor){
+					Instantiate(stone, new Vector3(x,y), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
